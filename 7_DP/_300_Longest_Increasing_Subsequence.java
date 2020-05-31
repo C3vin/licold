9794079cf307e21aa0a82d82a1e246ffd2f1efd9
The Bear @ Austin <46735920+zebointexas@users.noreply.github.com>
2020-05-31 11:50:05 -0500

    u

--- 7_DP/_300_Longest_Increasing_Subsequence.java
+++ 7_DP/_300_Longest_Increasing_Subsequence.java
@@ -1,22 +1,33 @@
-////////////////////////////////////////   DP
-class Solution {
-  
-        public int lengthOfLIS(int[] nums) {
-
-            int[] dp = new int[nums.length];
+public class Solution {
+    
+    public int lengthOfLIS(int[] nums) {
+        
+        if (nums.length == 0) return 0;
+      
+        int[] dp = new int[nums.length];
+        
+        dp[0] = 1;
+        
+        int maxAns = 1;
+        
+        int maxVal = 0; 
+        
+        for (int i = 1; i < dp.length; i++) {
             
-            int len = 0;
+            maxVal = 0;
             
-            for (int x : nums) {
-
-                int index = Arrays.binarySearch(dp, 0, len, x);
+            for (int j = 0; j < i; j++) {
                 
-                if (index < 0) index = -(index + 1);
-                
-                dp[index] = x;
-                
-                if (index == len) len++;    
+                if (nums[j] < nums[i])  maxVal = Math.max(maxVal, dp[j]);
+    
             }
-            return len;
+            
+            dp[i] = maxVal + 1; 
+            
+            maxAns = Math.max(maxAns, dp[i]); // 最后跟老大比
+            
         }
+        
+        return maxAns;  
+    }
 }
