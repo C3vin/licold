9318821db85d45685262e4ee681c69e246e0d2ab
The Bear @ Austin <46735920+zebointexas@users.noreply.github.com>
2020-09-16 16:45:56 -0500

    update

--- 19_Others/Bit_Manipulation/_268_Missing_Number.java
+++ 19_Others/Bit_Manipulation/_268_Missing_Number.java
@@ -4,16 +4,12 @@ public class Solution {
     
     public int missingNumber(int[] nums) {
         
-        int ans = nums.length;
+        int ans = 0; 
         
-        for (int i = 0; i < nums.length; i ++) {
-            
-            ans ^= nums[i];
-            
-            ans ^= i;
-            
-        }
+        for (int i = 0; i <= nums.length; i ++)  ans ^= i;
+        for (int i = 0; i <  nums.length; i ++)  ans ^= nums[i];
         
         return ans;
     }
 }
+
